e706ab756ee83435f3fa798f50694bbb5d81b2b1
EvrythingBurrito <perrizozach@yahoo.com>
2023-07-13 21:27:18 -0500

    basic code update

---
+++ source/Test.java
@@ -0,0 +1,10 @@
+package source;
+
+class Test
+{
+    public static void main(String []args)
+    {
+        System.out.println("My First Java Program.");
+    }
+};
+
---
+++ source/game_environment.java
@@ -0,0 +1,20 @@
+
+package source;
+
+class game_environment {
+
+    enum state {
+        LSE,
+        SSE,
+        LME,
+        NPCI,
+        COMBAT
+    }
+
+	public void run() {
+        // load gamedata associated with mode from database
+        // initialize mode menu
+        
+    }
+
+};
