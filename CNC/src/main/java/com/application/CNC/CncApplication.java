917363f644f46044706cdf2157a3e891a9e5106c
EvrythingBurrito <perrizozach@yahoo.com>
2024-06-08 18:19:33 -0500

    todo changes

--- CNC/src/main/java/com/application/CNC/CncApplication.java
+++ CNC/src/main/java/com/application/CNC/CncApplication.java
@@ -5,7 +5,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class CncApplication {
-
 	public static void main(String[] args) {
 		SpringApplication.run(CncApplication.class, args);
 	}
